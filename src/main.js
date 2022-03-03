e29f82fca35f54f4a7271b809c2261a092685119
Juansantoss07 <juansantosscunha@gmail.com>
2022-03-03 20:36:19 -0300

    update responsive and scroll reveal

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ScrollReveal from 'scrollreveal'
 
 createApp(App).mount('#app')
+
+ScrollReveal({
+    distance: '60px',
+    duration: 2500, 
+    delay: 400
+});
+
+ScrollReveal().reveal('.about-img', {delay:400, origin:'left'});
+ScrollReveal().reveal('.info-about', {delay:400, origin:'right'});
+ScrollReveal().reveal('.content-header', {delay:400, origin:'top'});
+ScrollReveal().reveal('.sociais-links', {delay:200, origin:'left'});
+ScrollReveal().reveal('.profile-img', {delay:200});
+ScrollReveal().reveal('.info', {delay:400, origin:'right'});
+ScrollReveal().reveal('.nav-links', {delay:100, origin:'bottom'});
+
