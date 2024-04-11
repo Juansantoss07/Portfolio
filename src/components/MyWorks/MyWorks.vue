77593f5be8a76d879810a29d5ff0a44b6422443a
Juansantoss07 <juansantosscunha@gmail.com>
2024-04-11 00:11:18 -0300

    adicionado novos works

--- src/components/MyWorks/MyWorks.vue
+++ src/components/MyWorks/MyWorks.vue
@@ -12,28 +12,22 @@ export default {
     <div class="box-myworks">
       <div class="works">
         <div id="card1" class="work-card">
-          <div class="img-work">
-            <img
-              src="/public/imgs/RocketCard.png"
-              alt=""
-            />
+          <div class="video-work">
+            <video src="/public/videos/ProjetoLaravels.mp4" autoplay loop> </video>
           </div>
-          <h1>RocketCard</h1>
+          <h1>Laravels Crud</h1>
           <div class="btns-work">
-            <a href="https://rocketcard-delta.vercel.app/" target="_blank">
+            <a href="#!">
               <button class="btn-cont">Acessar Site</button>
             </a>
-            <a href="https://github.com/Juansantoss07/rocketcard" target="_blank">
+            <a href="https://github.com/Juansantoss07/laravel9" target="_blank">
               <button class="btn-cont">Código GitHub</button>
             </a>
           </div>
         </div>
         <div id="card2" class="work-card">
           <div class="img-work">
-            <img
-              src="/public/imgs/JaneDoe.png"
-              alt=""
-            />
+            <img src="/public/imgs/JaneDoe.png" alt="" />
           </div>
           <h1>Projeto JaneDoe</h1>
           <div class="btns-work">
@@ -47,10 +41,7 @@ export default {
         </div>
         <div id="card3" class="work-card">
           <div class="img-work">
-            <img
-              src="/public/imgs/RocketFlix.png"
-              alt=""
-            />
+            <img src="/public/imgs/RocketFlix.png" alt="" />
           </div>
           <h1>RocketFlix</h1>
           <div class="btns-work">
@@ -67,27 +58,21 @@ export default {
       <div class="works">
         <div id="card1" class="work-card">
           <div class="img-work">
-            <img
-              src="/public/imgs/Clock.png"
-              alt=""
-            />
+            <img src="/public/imgs/RocketCard.png" alt="" />
           </div>
-          <h1>Projeto Clock</h1>
+          <h1>RocketCard</h1>
           <div class="btns-work">
-            <a href="http://responsive-clock-ui.vercel.app/" target="_blank">
+            <a href="https://rocketcard-delta.vercel.app/" target="_blank">
               <button class="btn-cont">Acessar Site</button>
             </a>
-            <a href="https://github.com/Juansantoss07/ClockUI" target="_blank">
+            <a href="https://github.com/Juansantoss07/rocketcard" target="_blank">
               <button class="btn-cont">Código GitHub</button>
             </a>
           </div>
         </div>
         <div id="card2" class="work-card">
           <div class="img-work">
-            <img
-              src="/public/imgs/Movi.png"
-              alt=""
-            />
+            <img src="/public/imgs/Movi.png" alt="" />
           </div>
           <h1>JCS Movi</h1>
           <div class="btns-work">
@@ -101,10 +86,7 @@ export default {
         </div>
         <div id="card3" class="work-card">
           <div class="img-work">
-            <img
-              src="/public/imgs/RocketNFT.png"
-              alt=""
-            />
+            <img src="/public/imgs/RocketNFT.png" alt="" />
           </div>
           <h1>Rockseat NTF's</h1>
           <div class="btns-work">
@@ -117,8 +99,56 @@ export default {
           </div>
         </div>
       </div>
+
+      <div class="works">
+        <div id="card1" class="work-card">
+          <div class="img-work">
+            <img src="/public/imgs/Clock.png" alt="" />
+          </div>
+          <h1>Projeto Clock</h1>
+          <div class="btns-work">
+            <a href="http://responsive-clock-ui.vercel.app/" target="_blank">
+              <button class="btn-cont">Acessar Site</button>
+            </a>
+            <a href="https://github.com/Juansantoss07/ClockUI" target="_blank">
+              <button class="btn-cont">Código GitHub</button>
+            </a>
+          </div>
+        </div>
+
+        <div id="card2" class="work-card">
+          <div class="img-work">
+            <img src="/public/imgs/decodificador.png" alt="Projeto decodificador de texto"/>
+          </div>
+          <h1>Decodificador de texto</h1>
+          <div class="btns-work">
+            <a href="https://juansantoss07.github.io/decodificadorDeTexto/" target="_blank">
+              <button class="btn-cont">Acessar Site</button>
+            </a>
+            <a href="https://github.com/Juansantoss07/decodificadorDeTexto" target="_blank">
+              <button class="btn-cont">Código GitHub</button>
+            </a>
+          </div>
+        </div>
+
+        <div id="card3" class="work-card">
+          <div class="img-work">
+            <img src="/public/imgs/weather-app.png" alt="Projeto decodificador de texto"/>
+          </div>
+          <h1>Weather App</h1>
+          <div class="btns-work">
+            <a href="https://weather-app-chi-five-10.vercel.app/" target="_blank">
+              <button class="btn-cont">Acessar Site</button>
+            </a>
+            <a href="https://github.com/Juansantoss07/WeatherApp" target="_blank">
+              <button class="btn-cont">Código GitHub</button>
+            </a>
+          </div>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" src="./style.scss" scoped/>
+<style lang="scss" src="./style.scss" scoped />
